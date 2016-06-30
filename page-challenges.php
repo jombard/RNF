313c4d18ce80215d107ffbcfa7e8a025ef051460
Jon Lombard <jombard@hotmail.com>
2016-06-30 21:00:17 +0100

    move challenge page script to external file

--- page-challenges.php
+++ page-challenges.php
@@ -255,44 +255,5 @@ get_header(); ?>
 		</div><!-- .col-md-4 -->
 	</div><!-- .row -->
 
-	<script type="text/javascript">
-	    //Isotope Filter
-	    var $container = $('.portfolio');
-	    $container.isotope({
-	        filter: '*',
-	        animationOptions: {
-	            duration: 750,
-	            easing: 'linear',
-	            queue: false
-	        }
-	    });
-
-	    $('nav.primary ul a').click(function () {
-	        var selector = $(this).attr('data-filter');
-	        $container.isotope({
-	            filter: selector,
-	            animationOptions: {
-	                duration: 750,
-	                easing: 'linear',
-	                queue: false
-	            }
-	        });
-	        return false;
-	    });
-
-	    var $optionSets = $('nav.primary ul'),
-	    $optionLinks = $optionSets.find('a');
-
-	    $optionLinks.click(function () {
-	        var $this = $(this);
-	        if ($this.hasClass('selected')) {
-	            return false;
-	        }
-	        var $optionSet = $this.parents('nav.primary ul');
-	        $optionSet.find('.selected').removeClass('selected');
-	        $this.addClass('selected');
-	    });
-	</script>
-
 	<!-- PAGE END -->
 <?php get_footer(); ?>
--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,41 @@ $(document).ready(function(){
 		"title": function(){return $(this).parent().parent().find("figcaption").text();}
 	});
 	$("a[rel^='prettyPhoto']").prettyPhoto();
+
+	//Isotope Filter
+    var $container = $('.portfolio');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('nav.primary ul a').click(function () {
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+
+    var $optionSets = $('nav.primary ul'),
+    $optionLinks = $optionSets.find('a');
+
+    $optionLinks.click(function () {
+        var $this = $(this);
+        if ($this.hasClass('selected')) {
+            return false;
+        }
+        var $optionSet = $this.parents('nav.primary ul');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+    });
 });
