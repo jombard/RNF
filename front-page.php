b96c2280f38d60f868b7700a80860b20b45bd7ce
Jon Lombard <jombard@hotmail.com>
2016-12-23 22:27:24 +0000

    Convert twitter script to secure delivery

--- front-page.php
+++ front-page.php
@@ -57,8 +57,9 @@ get_header(); ?>
 		</div>
 
 		<div class="col-md-5 hidden-xs">
-			<a class="twitter-timeline" data-width="520" data-height="800" href="https://twitter.com/RNF_UK">Tweets by RNF_UK</a>
-			<script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a class="twitter-timeline" data-width="520" data-height="800" href="https://twitter.com/RNF_UK" data-widget-id="705778350770556928">Tweets by @RNF_UK</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
 	    </div>
 	    
 	</div>
