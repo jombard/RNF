9d0f7e9412fc0334ec78346d71af0ca1d2bb926b
Jon Lombard <jombard@hotmail.com>
2017-03-02 21:46:34 +0000

    Update analytics code and move to header

--- footer.php
+++ footer.php
@@ -24,19 +24,6 @@
 	    </footer>
 
 	    <?php wp_footer(); ?>
-        
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date(); a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-46963552-1', 'rememberingnottoforget.org.uk');
-            ga('send', 'pageview');
-            
-        </script>
 
         <script src="/wp-content/themes/rnf/scripts/jquery-1.10.2.min.js"></script>
 		<script src="/wp-content/themes/rnf/scripts/bootstrap.min.js"></script>
