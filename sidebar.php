9153b420e15fa6c832ab48fc01e4700be11c6f4d
Jon Lombard <jombard@hotmail.com>
2017-07-21 20:03:19 +0100

    Improve sign up form style and text

--- sidebar.php
+++ sidebar.php
@@ -1,80 +1,83 @@
 <!-- SIDEBAR START -->
-
-<h3>Sign up to our newsletter</h3>
-<!-- Begin MailChimp Signup Form -->
-<div id="mc_embed_signup" class="panel">
-    <form action="https://rememberingnottoforget.us3.list-manage2.com/subscribe/post?u=4bbf97a920c7865dcf7b097ba&amp;id=f077e514d1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <p>We will look after your email address and you can unsubscribe very easily at any time.</p>
-        <div class="form-group">
-            <input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="email address" required>
-        </div>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;">
-                <input type="text" name="b_4bbf97a920c7865dcf7b097ba_f077e514d1" value="">
-        </div>
-        <div class="clear">
-            <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-info">Subscribe</button>
-            <div class="btn-group">
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    Previous Newsletters <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        <a href="http://eepurl.com/O1zzX" target="_blank">
-                            February 2014
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/Siyxr" target="_blank">
-                            April 2014
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/U_nC9" target="_blank">
-                            May 2014
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/Yrcof" target="_blank">
-                            July 2014
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/4xJ7P" target="_blank">
-                            September 2014
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/bcBPJL" target="_blank">
-                            February 2015
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/bqKocH" target="_blank">
-                            July 2015
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/bL2tlL" target="_blank">
-                            January 2016
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/cd_isD" target="_blank">
-                            September 2016
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://eepurl.com/cu1JfP" target="_blank">
-                            December 2016
-                        </a>
-                    </li>
-                </ul>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        Sign up to our newsletter
+    </div>
+    <!-- Begin MailChimp Signup Form -->
+    <div id="mc_embed_signup" class="panel-body">
+        <form action="https://rememberingnottoforget.us3.list-manage2.com/subscribe/post?u=4bbf97a920c7865dcf7b097ba&amp;id=f077e514d1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div class="form-group">Join our newsletter and get occasional updates on our challenges and fundraising distributions.</div>
+            <div class="form-group">
+                <input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="email address" required>
             </div>
-        </div>
-    </form>
-</div>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;">
+                <input type="text" name="b_4bbf97a920c7865dcf7b097ba_f077e514d1" value="">
+            </div>
+            <div class="clear">
+                <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-info">Subscribe</button>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        Previous Newsletters <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="http://eepurl.com/O1zzX" target="_blank">
+                                February 2014
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/Siyxr" target="_blank">
+                                April 2014
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/U_nC9" target="_blank">
+                                May 2014
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/Yrcof" target="_blank">
+                                July 2014
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/4xJ7P" target="_blank">
+                                September 2014
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/bcBPJL" target="_blank">
+                                February 2015
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/bqKocH" target="_blank">
+                                July 2015
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/bL2tlL" target="_blank">
+                                January 2016
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/cd_isD" target="_blank">
+                                September 2016
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://eepurl.com/cu1JfP" target="_blank">
+                                December 2016
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </form>
+    </div>
 <!--End mc_embed_signup-->
+</div>
 
 <h3>Join us on social media</h3>
 <a href="https://www.facebook.com/pages/Remembering-Not-To-Forget/235169236658679">
