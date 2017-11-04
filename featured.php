c9f014242f3ee98b449eaaaec221f868440baf0b
Jon Lombard <jombard@hotmail.com>
2017-11-04 17:26:04 +0000

    Add img-small class for loading

--- featured.php
+++ featured.php
@@ -2,7 +2,7 @@
 	<div class="p-b">
 		<a href="<?php the_permalink(); ?>">
 			<div class="placeholder" data-large="<?php the_post_thumbnail_url('full'); ?>">
-				<?php if ( has_post_thumbnail() ) { the_post_thumbnail('rnf-featured-thumb', ['class' => 'img-responsive m-auto']); } ?>
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail('rnf-featured-thumb', ['class' => 'img-responsive m-auto img-small']); } ?>
 				<div style="padding-bottom: 66.6%; width:200px;"></div>
 			</div>
 		</a>
