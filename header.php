7b98898bb96fa152bd8252abe9bee10618298dbd
Jon Lombard <jombard@hotmail.com>
2017-11-05 22:12:47 +0000

    Move menu toggle to center

--- header.php
+++ header.php
@@ -86,8 +86,8 @@
 
     <nav id="navbar" class="navbar navbar-rnf">
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
+            <div class="navbar-header text-center">
+                <button type="button" class="navbar-toggle collapsed" style="float:none;" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
