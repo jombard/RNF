4aadacd2a4b2a263c36e3e7630048e73093ae960
Jon Lombard <jombard@hotmail.com>
2020-08-05 22:26:35 +0100

    Remove challenges

--- front-page.php
+++ front-page.php
@@ -50,129 +50,62 @@ get_header(); ?>
 <div class="bg-rnf m-t-b">
 	<div class="container">
 		<div class="row text-center" style="color: #fff; padding: 40px 0;">
-			<div class="col-sm-4">
-				<h3 class="strong text-white">Challenges<br />Undertaken</h3>
-				<p class="h1"><?php echo get_post_meta($post->ID, 'challenges-undertaken', true); ?></p>
-			</div>
-			<div class="col-sm-4">
-				<h3 class="strong text-white">Amount<br />Raised</h3>
-				<p class="h1"><?php echo get_post_meta($post->ID, 'amount-raised', true); ?></p>
-			</div>
-			<div class="col-sm-4">
+			<div class="col-sm-6">
 				<h3 class="strong text-white">Organisations<br />Supported</h3>
 				<p class="h1"><?php echo get_post_meta($post->ID, 'organisations-supported', true); ?></p>
+			</div>
+			<div class="col-sm-6">
+				<h3 class="strong text-white">Amount<br />Raised</h3>
+				<p class="h1"><?php echo get_post_meta($post->ID, 'amount-raised', true); ?></p>
 			</div>
 		</div>
 	</div>
 </div>
 
 <div class="container">
-	<div class="row">
-		<div class="col-md-6">
+			<h2>Latest Blog Posts</h2>
 
-	        <h2>Latest Blog Posts</h2>
-
+				<div class="row">
 	        <?php
-		        $args = array('numberposts' => '3', 'post_status' => array('publish'));
+		        $args = array('numberposts' => '4', 'post_status' => array('publish'));
 		        $recent_posts = wp_get_recent_posts($args); ?>
-			    	<?php foreach ( $recent_posts as $recent ) : setup_postdata( $recent ); ?>
+						<?php foreach ( $recent_posts as $recent ) : setup_postdata( $recent ); ?>
+							<div class="col-md-6">
+
 			          <div class="row m-b">
-			            	<div class="col-sm-6">
+									<div class="col-sm-6">
 				        		<a href="<?php echo get_permalink($recent["ID"]) ?>">
-									<div class="placeholder" data-large="<?php echo get_the_post_thumbnail_url($recent['ID']); ?>">
-										<?php echo get_the_post_thumbnail($recent["ID"], 'rnf-featured-thumb', array( 'class' => "img-responsive m-auto img-small")); ?>  
-										<div style="padding-bottom: 66.6%; width:200px;"></div>
-									</div>
-								</a>
+											<div class="placeholder" data-large="<?php echo get_the_post_thumbnail_url($recent['ID']); ?>">
+												<?php echo get_the_post_thumbnail($recent["ID"], 'rnf-featured-thumb', array( 'class' => "img-responsive m-auto img-small")); ?>  
+												<div style="padding-bottom: 66.6%; width:200px;"></div>
+											</div>
+										</a>
 				        	</div>
 				        	<div class="col-sm-6">
-							<h4><a href="<?php echo get_permalink($recent["ID"]); ?>"><?php echo $recent["post_title"]; ?></a></h4>
-							    <p><?php echo $recent["post_excerpt"]; ?></p>
-								<a href="<?php echo get_permalink($recent["ID"]); ?>" class="btn btn-info">
-									Read more
-									<em class="glyphicon glyphicon-chevron-right"></em>
-								</a>
-						  	</div>
-			            </div>
+										<h4><a href="<?php echo get_permalink($recent["ID"]); ?>"><?php echo $recent["post_title"]; ?></a></h4>
+							    	<p><?php echo $recent["post_excerpt"]; ?></p>
+										<a href="<?php echo get_permalink($recent["ID"]); ?>" class="btn btn-info">
+											Read more
+											<em class="glyphicon glyphicon-chevron-right"></em>
+										</a>
+						  		</div>
+								</div>
+									
+							</div>
 			    	<?php endforeach; ?>
 		        <?php 
 		        wp_reset_query();
 		    ?>
+			</div>
 
 		</div>
-
-		<div class="col-md-6">
-
-	        <h2>Latest Challenges</h2>
-
-	        <div class="row m-b">
-	        	<div class="col-sm-6">
-	        		<a href="/challenges/2018-challenges/raffle/">
-						<div class="placeholder" data-large="/wp-content/uploads/2018/04/3691478767_8a8f172d88_b.jpg">
-							<img src="/wp-content/uploads/2018/04/3691478767_8a8f172d88_b.jpg" alt="Raffle" class="img-small">
-							<div style="padding-bottom: 66.6%; width:300px;"></div>
-						</div>
-					</a>
-	        	</div>
-	        	<div class="col-sm-6">
-					<h4><a href="/challenges/2018-challenges/raffle/">Raffle</a></h4>
-				    <p>Many fantastic prizes on offer.</p>
-					<a href="/challenges/2018-challenges/raffle/" class="btn btn-info">
-						Read more
-						<em class="glyphicon glyphicon-chevron-right"></em>
-					</a>
-			  	</div>
-        	</div>
-        	<div class="row m-b">
-	        	<div class="col-sm-6">
-	        		<a href="/challenges/2017-challenges/round-world-challenge/">
-						<div class="placeholder" data-large="/wp-content/uploads/2016/12/roundtheworld-e1482518013750.jpg">
-							<img src="/wp-content/uploads/2016/12/roundtheworld-e1482518013750.jpg" alt="Round the World" class="img-small">
-							<div style="padding-bottom: 66.6%; width:300px;"></div>
-						</div>
-					</a>
-	        	</div>
-	        	<div class="col-sm-6">
-					<h4><a href="/challenges/2017-challenges/round-world-challenge/">Round the World Challenge</a></h4>
-				    <p>Covering the distance of the equator through self-powered travel.</p>
-					<a href="/challenges/2017-challenges/round-world-challenge/" class="btn btn-info">
-						Read more
-						<em class="glyphicon glyphicon-chevron-right"></em>
-					</a>
-			  	</div>
-        	</div>
-        	<div class="row m-b">
-	        	<div class="col-sm-6">
-	        		<a href="/challenges/2017-challenges/end-end-challenge/">
-						<div class="placeholder" data-large="/wp-content/uploads/2017/01/Landsendsignpost-e1484777710592.jpg">
-							<img src="/wp-content/uploads/2017/01/Landsendsignpost-e1484777710592.jpg" alt="Lands End" class="img-small">
-							<div style="padding-bottom: 66.6%; width:300px;"></div>
-						</div>
-					</a>
-	        	</div>
-	        	<div class="col-sm-6">
-					<h4><a href="/challenges/2017-challenges/end-end-challenge/">Land's End to John O'Groats</a></h4>
-				    <p>1000 miles cycle challenge.</p>
-					<a href="/challenges/2017-challenges/end-end-challenge/" class="btn btn-info">
-						Read more
-						<em class="glyphicon glyphicon-chevron-right"></em>
-					</a>
-			  	</div>
-        	</div>
-		</div>
-	    
-	</div>
-</div>
 
 <div class="bg-rnf m-t-b">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 text-center" style="color: #fff; padding: 40px 0;">
-				<h3 class="text-white">Make a difference by fundraising for Remembering Not to Forget</h3>
-				<a href="/fundraising/fundraising-for-us/" class="btn btn-info">
-					More Information <em class="glyphicon glyphicon-chevron-right"></em>
-				</a>
-				<a href="/fundraising/fundraising-for-us/our-amazing-fundraisers/" class="btn btn-link text-white">
+				<h3 class="text-white m-t">Make a difference by fundraising for Remembering Not to Forget</h3>
+				<a href="/fundraising/fundraising-for-us/our-amazing-fundraisers/" class="btn btn-info btn-lg">
 					Our fundraisers <em class="glyphicon glyphicon-chevron-right"></em>
 				</a>
 			</div>
